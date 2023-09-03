962d0b1c8b6be09e7a1493d35ca11d616e70ab31
jaykush42 <jaykush9161@gmail.com>
2023-09-03 18:38:12 +0530

    main commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import NewsApi from "./components/NewsApi";
+
+function App() {
+  return (
+    <div className="App">
+     <NewsApi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewsApi.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from 'react'
+import News from './News';
+import './NewsApi.css'
+
+function NewsApi() { 
+    const apiKey = '97aabfb35bb74a37bdfd13238bee1470';
+    
+    const [newsList, setNewsList] = useState([]);
+    const [query, setQuery] = useState('India');
+    
+    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2023-08-02&sortBy=publishedAt&apiKey=${apiKey}`;
+  
+    const queryInputRef = useRef(null);
+
+    useEffect(()=>{
+       fetchData();
+    }, [query])  
+      
+    async function fetchData(){
+       try
+       { const response = await fetch(apiUrl);
+        const jsonData  = await response.json();
+
+        setNewsList(jsonData.articles);
+       } catch(e){
+        console.log(e,'error occured');
+       }
+    }
+
+    function handleSubmit(event){
+      event.preventDefault();
+      let queryValue = queryInputRef.current.value;
+      setQuery(queryValue);  
+    }
+
+  return (
+    <div className='news-app'> 
+    <h1> News Daily</h1>
+      <form onSubmit={handleSubmit}>
+      <input className='query-inp' type="text" ref = {queryInputRef} />
+      <input className='btn-submit' onClick={handleSubmit} type='submit' value="Submit" />
+      </form>
+    <div className='news-block'>
+      { newsList ? newsList.map((news) => {  
+        return <News key={news.url} news={news} />
+      }):null}
+    </div> 
+    </div>
+  );
+}
+
+export default NewsApi
